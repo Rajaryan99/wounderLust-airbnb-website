19a4ddd70ff2ec3c0d5a69cc0b8269edf870255f
Rajaryan singh <raj.aryan2518@gmail.com>
2025-09-05 20:47:58 +0530

    ExpressError handling

--- app.js
+++ app.js
@@ -37,10 +37,10 @@ app.get('/', (req, res) => {
 });
 
 //index route
-app.get('/listings', async (req, res) => {
+app.get('/listings',  wrapAsync (async (req, res) => {
     const allListing = await listing.find({});
     res.render('listings/index', { allListing });
-});
+}));
 
 //new route
 app.get('/listings/new', (req, res) => {
@@ -48,14 +48,17 @@ app.get('/listings/new', (req, res) => {
 })
 
 //show route
-app.get('/listings/:id', async (req, res) => {
+app.get('/listings/:id',  wrapAsync(async (req, res) => {
     let { id } = req.params;
     const listingDetails = await listing.findById(id);
     res.render('listings/show', { listing: listingDetails })
-});
+}));
 
 // create new listings
 app.post('/listings', wrapAsync(async (req, res, next) => {
+    if(!req.body.listing){
+        throw new ExpressError(400, 'send valid data for listing')
+    }
     let newListing = new listing(req.body.listing);
     await newListing.save();
     res.redirect('/listings')
@@ -64,27 +67,27 @@ app.post('/listings', wrapAsync(async (req, res, next) => {
 
 
 //edit route
-app.get('/listings/:id/edit', async (req, res) => {
+app.get('/listings/:id/edit',  wrapAsync(async (req, res) => {
     let { id } = req.params;
     const listingDetails = await listing.findById(id);
     res.render('listings/edit.ejs', { listing: listingDetails })
-});
+}));
 
 // update route
-app.put('/listings/:id', async (req, res) => {
+app.put('/listings/:id',  wrapAsync(async (req, res) => {
     let { id } = req.params;
     await listing.findByIdAndUpdate(id, { ...req.body.listing });
     res.redirect(`/listings/${id}`);
-});
+}));
 
 // delete route
 
-app.delete('/listings/:id', async (req, res) => {
+app.delete('/listings/:id',  wrapAsync(async (req, res) => {
     let { id } = req.params;
     let deleteListing = await listing.findByIdAndDelete(id);
     console.log(deleteListing);
     res.redirect('/listings');
-})
+}));
 
 // app.get('/testListing', (req, res) => {
 //     let sampleListing = new listing({
@@ -100,10 +103,15 @@ app.delete('/listings/:id', async (req, res) => {
 //     res.send('data saved successfully');
 // });
 
+// app.all('*', (req, res, next) => {
+//     next(new ExpressError(404, 'Page Not Found!'));
+// });
+
 app.use((err, req, res, next) => {
-    res.send('Something went wrong', err.message);
-})
+    let { statusCode = 500, message = 'Something went wrong' } = err;
+    res.render('error.ejs', {message});
+});
 
 app.listen(port, () => {
-    console.log(`Server is running on http://localost:${port}`);
-})
+    console.log(`Server is running on http://localhost:${port}`);
+});
