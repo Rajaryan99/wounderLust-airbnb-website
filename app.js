59cd2ec11c1634bb58d1341472f66e1d021366c0
Rajaryan singh <raj.aryan2518@gmail.com>
2025-09-05 22:14:10 +0530

    Validation for Schema using Jio library

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ const path = require('path');
 const methodOverride = require('method-override');
 const ejsMate = require('ejs-mate');
 const wrapAsync = require('./utils/wrapAsync.js');
-const ExpressError = require('./utils/ExpressError.js')
+const ExpressError = require('./utils/ExpressError.js');
+const { listingSchema } = require('./schema.js');
 
 const port = 8080;
 
@@ -37,7 +38,7 @@ app.get('/', (req, res) => {
 });
 
 //index route
-app.get('/listings',  wrapAsync (async (req, res) => {
+app.get('/listings', wrapAsync(async (req, res) => {
     const allListing = await listing.find({});
     res.render('listings/index', { allListing });
 }));
@@ -48,7 +49,7 @@ app.get('/listings/new', (req, res) => {
 })
 
 //show route
-app.get('/listings/:id',  wrapAsync(async (req, res) => {
+app.get('/listings/:id', wrapAsync(async (req, res) => {
     let { id } = req.params;
     const listingDetails = await listing.findById(id);
     res.render('listings/show', { listing: listingDetails })
@@ -56,8 +57,10 @@ app.get('/listings/:id',  wrapAsync(async (req, res) => {
 
 // create new listings
 app.post('/listings', wrapAsync(async (req, res, next) => {
-    if(!req.body.listing){
-        throw new ExpressError(400, 'send valid data for listing')
+    let result = listingSchema.validate(req.body);
+    console.log(result);
+    if (result.error) {
+        throw new ExpressError(400, result.error);
     }
     let newListing = new listing(req.body.listing);
     await newListing.save();
@@ -67,14 +70,14 @@ app.post('/listings', wrapAsync(async (req, res, next) => {
 
 
 //edit route
-app.get('/listings/:id/edit',  wrapAsync(async (req, res) => {
+app.get('/listings/:id/edit', wrapAsync(async (req, res) => {
     let { id } = req.params;
     const listingDetails = await listing.findById(id);
     res.render('listings/edit.ejs', { listing: listingDetails })
 }));
 
 // update route
-app.put('/listings/:id',  wrapAsync(async (req, res) => {
+app.put('/listings/:id', wrapAsync(async (req, res) => {
     let { id } = req.params;
     await listing.findByIdAndUpdate(id, { ...req.body.listing });
     res.redirect(`/listings/${id}`);
@@ -82,7 +85,7 @@ app.put('/listings/:id',  wrapAsync(async (req, res) => {
 
 // delete route
 
-app.delete('/listings/:id',  wrapAsync(async (req, res) => {
+app.delete('/listings/:id', wrapAsync(async (req, res) => {
     let { id } = req.params;
     let deleteListing = await listing.findByIdAndDelete(id);
     console.log(deleteListing);
@@ -109,7 +112,7 @@ app.delete('/listings/:id',  wrapAsync(async (req, res) => {
 
 app.use((err, req, res, next) => {
     let { statusCode = 500, message = 'Something went wrong' } = err;
-    res.render('error.ejs', {message});
+    res.render('error.ejs', { message });
 });
 
 app.listen(port, () => {
