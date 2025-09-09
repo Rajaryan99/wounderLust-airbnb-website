3280290860c9d100306892c9ba76a6d049cb0de3
Rajaryan singh <raj.aryan2518@gmail.com>
2025-09-09 22:14:10 +0530

    added a review model in database

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.get('/', (req, res) => {
 });
 
 const validateListing = (req, res, next) => {
-     let {error} = listingSchema.validate(req.body);
+    let { error } = listingSchema.validate(req.body);
     if (error) {
         throw new ExpressError(400, error);
     } else {
@@ -81,7 +81,7 @@ app.get('/listings/:id/edit', wrapAsync(async (req, res) => {
 }));
 
 // update route
-app.put('/listings/:id',validateListing,  wrapAsync(async (req, res) => {
+app.put('/listings/:id', validateListing, wrapAsync(async (req, res) => {
     let { id } = req.params;
     await listing.findByIdAndUpdate(id, { ...req.body.listing });
     res.redirect(`/listings/${id}`);
