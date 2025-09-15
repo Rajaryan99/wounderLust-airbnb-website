23009fd43f516bb4a55919c0de9d2739290d2685
Rajaryan singh <raj.aryan2518@gmail.com>
2025-09-15 21:58:30 +0530

    signUp page

--- app.js
+++ app.js
@@ -5,8 +5,6 @@ const methodOverride = require('method-override');
 const ejsMate = require('ejs-mate');
 const ExpressError = require('./utils/ExpressError.js');
 const session = require('express-session');
-const listings = require('./routes/listing.js');
-const reviews = require('./routes/review.js');
 const flash = require('connect-flash');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
@@ -15,6 +13,11 @@ const User = require('./models/user.auth.js');
 const app = express();
 
 const port = 8080;
+
+//Routers 
+const listingRouter = require('./routes/listing.js');
+const reviewRouter = require('./routes/review.js');
+const userRouter = require('./routes/user.js');
 
 
 app.use(express.urlencoded({ extended: true }));
@@ -78,21 +81,23 @@ app.use((req, res, next) => {
 });
 
 // creating a fake user
-app.get('/demouser', async(req, res) => {
+app.get('/demouser', async (req, res) => {
     let fakeUser = new User({
         email: "fakeuser@gmail.com",
         username: "fake-user",
     });
 
-   let registerUser = await User.register(fakeUser, "helloWorld") //pass the user and passWord
-   res.send(registerUser);
+    let registerUser = await User.register(fakeUser, "helloWorld") //pass the user and passWord
+    res.send(registerUser);
 });
 
 // Listing routes
-app.use('/listings', listings);
+app.use('/listings', listingRouter);
 
 //reviews routes
-app.use('/listings/:id/reviews', reviews);
+app.use('/listings/:id/reviews', reviewRouter);
+
+app.use('/', userRouter);
 
 
 
