364a69ecca4ff61370c4ad39a6d61b024c36a938
Rajaryan singh <raj.aryan2518@gmail.com>
2025-09-15 23:11:56 +0530

    user Authentication

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/user.auth.js');
 const wrapAsync = require('../utils/wrapAsync');
+const passport = require('passport');
 
 router.get('/signup', (req, res) => {
     res.render('users/signup.ejs');
@@ -26,6 +27,9 @@ router.get('/login', (req, res) => {
     res.render('users/login.ejs');
 });
 
-
+router.post('/login', passport.authenticate('local', { failureRedirect: '/login', failureFlash: true }), async (req, res) => {
+    req.flash('success', 'Welcome back to WounderLust!');
+    res.redirect('/listings')
+})
 
 module.exports = router;
