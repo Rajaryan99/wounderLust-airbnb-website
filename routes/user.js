bfc7aefbbe634bce0a65d6efd5fef3626626cec5
Rajaryan singh <raj.aryan2518@gmail.com>
2025-09-15 22:51:20 +0530

    user login and signup page/ authentication

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,31 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/user.auth.js');
+const wrapAsync = require('../utils/wrapAsync');
 
 router.get('/signup', (req, res) => {
     res.render('users/signup.ejs');
 });
+
+router.post('/signup', wrapAsync(async (req, res) => {
+    try {
+        let { username, email, password } = req.body;
+        const newUser = new User({ email, username });
+        const registeredUser = await User.register(newUser, password);
+        console.log(registeredUser);
+        req.flash('success', "Welcome to WounderLust!");
+        res.redirect('/listings');
+    } catch (error) {
+        req.flash('error', error.message);
+        res.redirect('/signup');
+    }
+
+}));
+
+router.get('/login', (req, res) => {
+    res.render('users/login.ejs');
+});
+
+
 
 module.exports = router;
