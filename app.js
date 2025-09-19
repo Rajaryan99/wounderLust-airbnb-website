747d8eb6ca0be630800258a37232f6a7faf8ca18
Rajaryan singh <raj.aryan2518@gmail.com>
2025-09-19 22:13:53 +0530

    dotenv file

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const flash = require('connect-flash');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const User = require('./models/user.auth.js');
+require('dotenv').config();
 
 const app = express();
 
